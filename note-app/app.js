e73efb7a5e15460bcccfedeabb75e4808a93b1dc
krzysztofk <krzysztofkicinger@gmail.com>
2017-08-22 15:26:08 +0200

    Clean up

--- note-app/app.js
+++ note-app/app.js
@@ -1,29 +1,6 @@
-const os = require('os');
 const fs = require('fs');
-const notes = require('./notes');
 const _ = require('lodash');
+const notes = require('notes');
 
 console.log('Starting app...');
 
-
-const user = os.userInfo();
-notes.addNote(`Hello ${user.username}`);
-
-const result = notes.add(1, 2);
-console.log(`Result: ${result}`);
-console.log(`Is string: ${_.isString(result)}`);
-console.log(`Create unique id: ${_.uniqueId()}`)
-
-const filteredArray = _.uniq(['Chris', 1, 'Chris', 1, 4, 3]);
-console.log(`Array: ${filteredArray}`);
-
-
-// appendFile(fileName, `\nHello + ${user.username}`);
-
-// function appendFile(file, text) {
-//     fs.appendFile(file, text, function(error) {
-//         if(error) {
-//             console.log('Unable to write to file');
-//         }
-//     });
-// }
--- note-app/notes.js
+++ note-app/notes.js
@@ -1,7 +1,3 @@
-console.log('Starting notes.js...');
-
 module.exports.addNote = (note) => {
     console.log(`Added note: ${note}`);
 };
-
-module.exports.add = (a, b) => a + b;
