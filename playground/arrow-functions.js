918501de229dda9edb3777bc7ce7a5a757867ecd
krzysztofkicinger <krzysztofkicinger@gmail.com>
2017-08-23 21:02:39 +0200

    Added arrow-functions file

---
+++ playground/arrow-functions.js
@@ -0,0 +1,15 @@
+/**
+ * Created by krzysztofkicinger on 23/08/2017.
+ */
+
+const square = x => x * x;
+console.log(square(9));
+
+const user = {
+    name: 'Chris',
+    sayHi: () => {
+        console.log('Hi');
+    }
+};
+
+user.sayHi();
