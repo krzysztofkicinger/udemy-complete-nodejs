b6e4e5892423284bc520babb45aa3874cc896460
krzysztofk <krzysztofkicinger@gmail.com>
2017-08-24 15:20:50 +0200

    [weather-app] Geocode changed to axios (comparison with request)

--- weather-app/api/geocode/geocode.js
+++ weather-app/api/geocode/geocode.js
@@ -1,4 +1,5 @@
 const request = require('request');
+const axios = require('axios');
 const {
     encodeAddress,
     isOkRequest,
@@ -6,30 +7,50 @@ const {
 } = require('../api-helpers');
 
 module.exports.geocodeAddress = (address) => {
-    return new Promise((resolve, reject) => {
-        request.get({
-            url: 'https://maps.googleapis.com/maps/api/geocode/json',
-            qs: {
-                address: encodeAddress(address)
-            },
-            json: true
-        }, (error, response, body) => {
-            if(error) {
-                reject('Unable to connect to Google servers');
-            }
-            if(isOkRequest(response)) {
-                if(isAtLeastOneResult(body)) {
-                    const { formatted_address, geometry: { location: { lat, lng } } } = body.results[0];
-                    resolve({
-                        address: formatted_address,
-                        latitude: lat,
-                        longitude: lng
-                    });
-                } else {
-                    reject('Unable to find that address');
-                }
-            }
-        });
+    return axios.get('https://maps.googleapis.com/maps/api/geocode/json', {
+        params : {
+            address: encodeAddress(address)
+        }
+    }).then(response => {
+        if(isOkRequest(response) && !isAtLeastOneResult(response.data)) {
+           throw new Error('Unable to find that address');
+        } else {
+            const { formatted_address, geometry: { location: { lat, lng } } } = response.data.results[0];
+            return {
+                address: formatted_address,
+                latitude: lat,
+                longitude: lng
+            };
+        }
+    }).catch(error => {
+        console.log(error)
     });
-
 };
+
+// module.exports.geocodeAddress = (address) => {
+//     return new Promise((resolve, reject) => {
+//         request.get({
+//             url: 'https://maps.googleapis.com/maps/api/geocode/json',
+//             qs: {
+//                 address: encodeAddress(address)
+//             },
+//             json: true
+//         }, (error, response, body) => {
+//             if(error) {
+//                 reject('Unable to connect to Google servers');
+//             }
+//             if(isOkRequest(response)) {
+//                 if(isAtLeastOneResult(body)) {
+//                     const { formatted_address, geometry: { location: { lat, lng } } } = body.results[0];
+//                     resolve({
+//                         address: formatted_address,
+//                         latitude: lat,
+//                         longitude: lng
+//                     });
+//                 } else {
+//                     reject('Unable to find that address');
+//                 }
+//             }
+//         });
+//     });
+// };
