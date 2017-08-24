3d0e341ec1d91aa243ecdc3f5bac27aedb96792a
krzysztofk <krzysztofkicinger@gmail.com>
2017-08-24 11:08:20 +0200

    [weather-app] Moved Google API geocode functionality to another module

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,4 +1,4 @@
-const request = require('request');
+const geocode = require('./geocode/geocode');
 const yargs = require('yargs');
 
 const argv = yargs
@@ -14,34 +14,4 @@ const argv = yargs
     .alias('h', 'help')
     .argv;
 
-request.get({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json',
-    qs: {
-        address: encodeURIComponent(argv.address)
-    },
-    json: true
-}, (error, response, body) => {
-    if(error) {
-        console.log('Unable to connect to Google servers', error);
-    }
-    if(isOkRequest(response)) {
-        if(isAtLeastOneResult(body)) {
-            console.log(`Formatted address: ${body.results[0].formatted_address}`)
-            console.log(`[Lat, Lng]: ${body.results[0].geometry.location.lat}, ${body.results[0].geometry.location.lng}`)
-        } else {
-            console.log(response);
-        }
-    }
-});
-
-const prettyStringify = (object) => {
-    return JSON.stringify(object, undefined, 4);
-};
-
-const isOkRequest = (request) => {
-    return request.statusCode === 200;
-};
-
-const isAtLeastOneResult = (body) => {
-    return !(body.status === 'ZERO_RESULTS');
-};
+geocode.geocodeAddress(argv.address);
