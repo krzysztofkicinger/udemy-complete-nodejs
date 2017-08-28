9f5a37fa661b40936a0e80120b975ca91d073148
krzysztofk <krzysztofkicinger@gmail.com>
2017-08-28 08:42:14 +0200

    [tests-app] Assertion examples using expect (toBeA, toInclude, toExclude, toNotBeA)

--- tests-app/utils/utils.js
+++ tests-app/utils/utils.js
@@ -1,3 +1,9 @@
 module.exports.add = (a, b) => a + b;
 
 module.exports.square = (x) => Math.pow(x, 2);
+
+module.exports.setName = (user, fullName) => {
+    const names = fullName.split(' ');
+    user.firstName = names[0];
+    user.lastName = names[1];
+};
--- tests-app/utils/utils.test.js
+++ tests-app/utils/utils.test.js
@@ -10,3 +10,41 @@ it('Should square a number', () => {
     const result = utils.square(2);
     expect(result).toBeA('number').toBe(4);
 });
+
+it('Should expect some values', () => {
+    expect(12).toNotBe(11);
+
+    // For objects we need to use toEqual
+    // expect({ name: 'Andrew' }).toBe({ name: 'Andrew' });
+    expect({ name: 'Andrew' }).toEqual({ name: 'Andrew' });
+
+    expect([2, 3, 4]).toInclude(2);
+    expect([2, 3, 4]).toExclude(5);
+
+    // Expecting an object to contain particular property
+    expect({
+        name: 'Andrew',
+        age: 25,
+        location: 'Philadelphia'
+    }).toInclude({
+        age: 25
+    }).toExclude({
+        name: 'John'
+    });
+});
+
+it('Should have first name and last name with proper values', () => {
+    const firstName = 'John';
+    const lastName = 'Smith';
+    const fullName = `${firstName} ${lastName}`;
+    const user = {age: 25, location: 'Chicago'};
+    utils.setName(user, fullName);
+    expect(user)
+        .toBeA('object')
+        .toInclude({
+            firstName,
+            lastName,
+            age: 25,
+            location: 'Chicago'
+        });
+});
