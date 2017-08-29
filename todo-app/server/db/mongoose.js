976b1bd047bda605e2babd8dda5c78d0786e95b2
krzysztofk <krzysztofkicinger@gmail.com>
2017-08-29 09:46:26 +0200

    [todo-app] Added endpoints (/todos - POST, GET, /todos/:id - GET)

--- todo-app/server/db/mongoose.js
+++ todo-app/server/db/mongoose.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
+const winston = require('winston');
 
 // Mongoose provides both callbacks and Promise communication, choosing Promise
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+process.on('exit', () => {
+    mongoose.disconnect();
+});
 
 module.exports = mongoose;
--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -1,4 +1,35 @@
 const mongoose = require('./db/mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
 const Todo = require('./model/Todo');
 const User = require('./model/User');
+const winston = require('winston');
 
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (request, response) => {
+    new Todo({ text: request.body.text }).save()
+        .then(todo => {
+            winston.info('Todo has been created', todo);
+            response.status(201).send(todo);
+        })
+        .catch(error => {
+            winston.error('Error occured during todo creation', error);
+            response.status(400).send(error);
+        });
+});
+
+app.get('/todos', (request, response) => {
+
+});
+
+app.get('/todos/:id', (request, response) => {
+    console.log(`Id: `, request.params.id);
+});
+
+app.listen(3000, () => {
+    winston.info(`Server is listening on port ${3000}`)
+});
