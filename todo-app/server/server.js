3accbfcd0ae24ef9e89af903b2f4194aca5de538
krzysztofk <krzysztofkicinger@gmail.com>
2017-08-29 08:52:13 +0200

    [todo-app] Created User and Todo model

--- todo-app/server/server.js
+++ todo-app/server/server.js
@@ -1,27 +1,38 @@
 const mongoose = require('mongoose');
 const Todo = require('./model/Todo');
+const User = require('./model/User');
 
 // Mongoose provides both callbacks and Promise communication
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-const newTodo = new Todo({
-    text: 'Cook dinner'
+// const newTodo = new Todo({
+//     text: '  Edit this video '
+// });
+//
+// newTodo.save().then(data => {
+//     console.log('Saved todo', data);
+// }, error => {
+//     console.log('Error during saving todo', error);
+// });
+
+// const completeTodo = new Todo({
+//     text: 'Go shopping',
+//     completed: false,
+//     completedAt: 1
+// });
+//
+// completeTodo.save()
+//     .then(data => console.log('Saved todo', data))
+//     .catch(error => console.log('Error occurred', error));
+
+const user = new User({
+    email: 'test@example.com'
 });
 
-newTodo.save().then(data => {
-    console.log('Saved todo', data);
-}, error => {
-    console.log('Error during saving todo', error);
-});
+user.save()
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
 
-const completeTodo = new Todo({
-    text: 'Go shopping',
-    completed: false,
-    completedAt: 1
-});
-
-completeTodo.save()
-    .then(data => console.log('Saved todo', data))
-    .catch(error => console.log('Error occurred', error));
+mongoose.disconnect();
