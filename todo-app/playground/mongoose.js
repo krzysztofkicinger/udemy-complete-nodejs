19f747e0e7566bba8ef503de8250218b2cf91f7c
krzysztofk <krzysztofkicinger@gmail.com>
2017-08-29 15:29:28 +0200

    [todo-app] ObjectID validation

--- todo-app/playground/mongoose.js
+++ todo-app/playground/mongoose.js
@@ -4,10 +4,12 @@ const Todo = require('../server/model/Todo');
 const User = require('../server/model/User');
 const winston = require('winston');
 
+console.log(`ObjectID Valid: ${ObjectID.isValid('59a54d865c2ab217940a7a88')}`)
+console.log(`ObjectID Valid: ${ObjectID.isValid('59a54d865c2ab275940a7a80')}`)
+
 Todo.find({
     _id: '59a54d865c2ab217940a7a88'
 }).then(todos => console.log(todos));
-
 
 Todo.findOne({
     _id: '59a54d865c2ab217940a7a88'
